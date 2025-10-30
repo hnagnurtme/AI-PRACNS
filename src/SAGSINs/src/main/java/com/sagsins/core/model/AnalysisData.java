473ecc359b5a29955945a3e68c5a231f0a2165a0
Trung Ánh <anhnon0106@gmail.com>
2025-10-30 13:40:40 +0700

    feat(monito-ui): split moudule, optimize batch monitor

---
+++ src/SAGSINs/src/main/java/com/sagsins/core/model/AnalysisData.java
@@ -0,0 +1,16 @@
+package com.sagsins.core.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AnalysisData {
+    private double avgLatency;
+    private double avgDistanceKm;
+    private double routeSuccessRate;
+    private double totalDistanceKm;
+    private double totalLatencyMs;
+}
---
+++ src/SAGSINs/src/main/java/com/sagsins/core/model/BufferState.java
@@ -0,0 +1,13 @@
+package com.sagsins.core.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BufferState {
+    private int queueSize;
+    private double bandwidthUtilization;
+}
