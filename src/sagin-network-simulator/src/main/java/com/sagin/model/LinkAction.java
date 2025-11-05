69e2740bbde607a819d459df540eae55b22cda88
Trung Ánh <anhnon0106@gmail.com>
2025-11-05 19:15:16 +0700

    chore(project): remove DRL agent and network simulator modules

--- src/sagin-network-simulator/src/main/java/com/sagin/model/LinkAction.java
+++
@@ -1,7 +0,0 @@
-package com.sagin.model;
-
-public enum LinkAction {
-    POWER_BOOST,
-    BEAM_STEERING,
-    ADJUST_FREQUENCY
-}
--- src/sagin-network-simulator/src/main/java/com/sagin/model/NodeType.java
+++
@@ -1,8 +0,0 @@
-package com.sagin.model;
-
-public enum NodeType {
-    GROUND_STATION,
-    LEO_SATELLITE,
-    MEO_SATELLITE,
-    GEO_SATELLITE
-}
